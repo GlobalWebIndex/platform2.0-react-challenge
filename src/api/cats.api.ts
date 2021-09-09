9701f802b9680338f67e4650147db2e9d5a97a2b
profanis <prodromouf@gmail.com>
2021-09-09 21:07:17 +0300

    list of breeds - Non styled

--- src/api/cats.api.ts
+++ src/api/cats.api.ts
@@ -1,9 +1,12 @@
 import axios from 'axios'
+import { BreedType } from '../types/Breed.type'
 import { CatType } from '../types/cat.type'
 import { SortingOrder } from '../types/Sorting-order.type'
 
 export default class CatsApi {
   private static readonly domain: string = 'https://api.thecatapi.com/v1'
+
+  private static readonly API_KEY = 'aa796a19-0f88-4237-87b7-73df20400b6b'
 
   // TODO: change to getImages
   static getCats(
@@ -25,7 +28,31 @@ export default class CatsApi {
     return axios
       .get<CatType>(`${this.domain}/images/${imageId}`, {
         headers: {
-          'x-api-key': 'aa796a19-0f88-4237-87b7-73df20400b6b',
+          'x-api-key': this.API_KEY,
+        },
+      })
+      .then(response => cb(response.data))
+  }
+
+  static getBreeds(
+    limit: number,
+    page: number,
+    cb: (data: BreedType[]) => void
+  ) {
+    return axios
+      .get<BreedType[]>(`${this.domain}/breeds?limit=${limit}&page=${page}`, {
+        headers: {
+          'x-api-key': this.API_KEY,
+        },
+      })
+      .then(response => cb(response.data))
+  }
+
+  static getBreed(breedId: string, cb: (data: BreedType[]) => void) {
+    return axios
+      .get<BreedType[]>(`${this.domain}/images/search?breed_id=${breedId}`, {
+        headers: {
+          'x-api-key': this.API_KEY,
         },
       })
       .then(response => cb(response.data))
--- src/views/Breeds/BreedsPage.tsx
+++ src/views/Breeds/BreedsPage.tsx
@@ -1,9 +1,42 @@
-import { Component } from 'react'
+import { Button } from '@chakra-ui/react'
+import { FC, useEffect, useState } from 'react'
+import CatsApi from '../../api/cats.api'
+import { BreedType } from '../../types/Breed.type'
 
-interface BreedsState {}
+const BreedsPage: FC = () => {
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(0)
+  const [breeds, setBreeds] = useState<BreedType[]>([])
 
-export default class BreedsPage extends Component<{}, BreedsState> {
-  render() {
-    return <p>This is the breeds page</p>
+  function nextPage() {
+    setPage(it => it + 1)
   }
+
+  /**
+   * This effect is responsible to load breeds via API
+   */
+  useEffect(() => {
+    CatsApi.getBreeds(limit, page, breedsResponse =>
+      setBreeds([...breeds, ...breedsResponse])
+    )
+
+    // clean up. Like we do with componentDidUpdate
+    return () => {}
+  }, [page])
+
+  return (
+    <>
+      <ul>
+        {breeds.map((breed, index) => (
+          <li>{breed.name}</li>
+        ))}
+      </ul>
+
+      <Button colorScheme='blue' onClick={() => nextPage()}>
+        Load more
+      </Button>
+    </>
+  )
 }
+
+export default BreedsPage
