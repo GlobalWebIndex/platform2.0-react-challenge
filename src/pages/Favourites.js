c75271001e515aac5970ec0eb7c39324215e31cd
Sotiris Bimpas <s.g.bibas@gmail.com>
2021-09-21 13:33:27 +0300

    add favourites page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { useAxios } from "./../hooks/useAxios";
+import OverlayLoader from "../components/OverlayLoader";
+import Gallery from "../components/Gallery";
+
 export default function Favourites() {
-    return <h1>Favourites</h1>
+  const [favourites, error, loading, fetch] = useAxios();
+
+  useEffect(() => {
+    if (!favourites) fetch({ url: "/favourites" });
+  }, [favourites, fetch]);
+
+  return (
+    <OverlayLoader active={loading}>
+      <Gallery cards={favourites} />
+    </OverlayLoader>
+  );
 }
