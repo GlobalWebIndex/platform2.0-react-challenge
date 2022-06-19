dda9344101c21fa45f9418d205c70c97b29dcf81
George Tsakiris <tsaki67@gmail.com>
2022-06-19 12:16:43 +0300

    feat: add axios default config with env variables

---
+++ src/services/Api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import qs from 'qs';
+
+axios.defaults.baseURL = process.env.REACT_APP_CAT_API_ENDPOINT;
+axios.defaults.headers.common['x-api-key'] =
+  process.env.REACT_APP_CAT_API_KEY || '';
+
+function assertData() {
+  return axios.create({
+    timeout: 0,
+    responseType: 'json',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    paramsSerializer: (params) => {
+      return qs.stringify(params, { arrayFormat: 'repeat' });
+    },
+  });
+}
+
+const Data = assertData();
+
+Data.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { Data };
